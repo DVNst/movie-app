15bd0f85dd0c494a0f5841e2f7b409061a5dc311
DVNst <dvn-studio@mail.ru>
2020-12-22 02:13:39 +0300

    add axias

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import Header from "./components/header/header.jsx";
 import MovieItem from "./components/movie-item/movie-item.jsx";
 import Footer from "./components/footer/footer.jsx";
 
-import { moviesData } from "./moviesData.js";
+// import { moviesData } from "./moviesData.js";
 
-console.log(moviesData);
+const API_KEY="6f6619cbc5b3e425c95dd474d5046eec";
+
+//Get Details
+//https://api.themoviedb.org/3/movie/ID?api_key=API_KEY
+
+//Get Alternative Titles
+//https://api.themoviedb.org/3/movie/299536/alternative_titles?api_key=6f6619cbc5b3e425c95dd474d5046eec
+//{"id": 299536, "titles": [{"iso_3166_1": "RU", "title": "Мстители: Война бесконечности. Часть 1", "type": ""}]}
+
+//Get актеры
+//https://api.themoviedb.org/3/movie/299536/credits?api_key=6f6619cbc5b3e425c95dd474d5046eec&language=en-US
+
+//get movies
+//https://api.themoviedb.org/3/discover/movie?api_key=6f6619cbc5b3e425c95dd474d5046eec&language=ru-RU&sort_by=popularity.desc&include_adult=false&page=1
+//language=en-US
+//sort_by=popularity.asc (популярность по возрастанию), popularity.desc (популярность по убыванию), vote_average.asc, vote_average.desc (рейтинг)
 
 function App() {
-  const [movies, setMovies] = useState(moviesData);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    // axios.get("http://localhost:3001/lists/?_expand=color&_embed=tasks").then(({ data }) => {
-    //   setMovies(moviesData);
-    // });
+    const API_REQUEST="https://api.themoviedb.org/3/discover/movie";
+    const language="ru-RU"
+    const sortingType=["popularity.asc", "popularity.desc", "vote_average.asc", "vote_average.desc", "release_date.asc", "release_date.desc", "primary_release_date.asc", "primary_release_date.desc"];
+    const page = 1;
 
-    setMovies(moviesData);
+    axios.get(`${API_REQUEST}?api_key=${API_KEY}&language=${language}&sort_by=${sortingType[1]}&include_adult=false&page=${page}`)
+      .then(({ data }) => {setMovies(data.results)});
   }, []);
+
+  useEffect(() => {
+    console.log(movies);
+  }, [movies]);
+
+  const addFavoriteMovie = (movie) => {
+    console.log("Favorite", movie.id, movie.title);
+  };
+
+  const addWatchMovie = (movie) => {
+    console.log("Watch", movie.id, movie.title);
+  };
 
   return (
     <>
       <Header />
       <ul className="movies">
-        {movies.map((movie, i) => (
+        {movies.length>0 && movies.map((movie, i) => (
           <MovieItem
             movie={movie}
             key={movie.id}
+            addFavoriteMovie={addFavoriteMovie}
+            addWatchMovie={addWatchMovie}
           />
         ))}
       </ul>
--- src/components/movie-item/movie-item.jsx
+++ src/components/movie-item/movie-item.jsx
@@ -2,25 +2,47 @@
 
 import "./movie-item.scss";
 
-const MovieItem = ({ movie }) => {
-  const { poster_path, original_title, release_date, vote_average } = movie;
-  const movieDate = new Date(release_date).getFullYear();
+const MovieItem = ({ movie, addFavoriteMovie, addWatchMovie }) => {
+  const { poster_path, title, original_title, release_date, vote_average } = movie;
+  const movieDate = release_date ? new Date(release_date).getFullYear() : "-";
   return (
     <li className="movie-card">
-      <a href="#" className="movie-card__link">
-        <img
-          className="movie-card__img"
-          width="220px"
-          height="330px"
-          src={`https://image.tmdb.org/t/p/w220_and_h330_face/${poster_path}`}
-          alt="" />
+      <a href="#" className="movie-card__link movie-card__poster">
+        {
+          poster_path &&
+          <img
+            className="movie-card__img"
+            width="220px"
+            height="330px"
+            src={`https://image.tmdb.org/t/p/w220_and_h330_face/${poster_path}`}
+            alt="" />
+        }
       </a>
 
       <p className="movie-card__title">
-        <a href="#" className="movie-card__link">{original_title}</a>
+        <a href="#" className="movie-card__link">
+          {title ? title : original_title}
+        </a>
       </p>
-      <p className="movie-card__text">{movieDate}</p>
-      <p className="movie-card__rating">{vote_average * 10}%</p>
+      <p className="movie-card__date">{movieDate}</p>
+
+      <div className="movie-card__indicators">
+        <p className="movie-card__rating">{vote_average * 10}%</p>
+        <button 
+          className="movie-card__btn movie-card__btn--favorite"
+          title="Добавить в Избранное"
+          onClick={() => addFavoriteMovie(movie)}
+        >
+          <span className="visually-hidden">Добывить в избранное</span>
+        </button>
+        <button
+          className="movie-card__btn movie-card__btn--watch"
+          title="Добавить в свой список отслеживания"
+          onClick={() => addWatchMovie(movie)}
+        >
+          <span className="visually-hidden">Добавить в свой список отслеживания</span>
+        </button>
+      </div>
     </li>
   );
 };
